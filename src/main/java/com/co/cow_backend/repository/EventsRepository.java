f367e491db56baa33d2880a5e5fe857a24de3288
avannesarestrepo <avannesarestrepo@gmail.com>
2022-10-10 22:46:50 -0500

    update

--- src/main/java/com/co/cow_backend/repository/EventsRepository.java
+++ src/main/java/com/co/cow_backend/repository/EventsRepository.java
@@ -10,7 +10,6 @@ import java.util.List;
 @Repository
 public interface EventsRepository extends JpaRepository<Events, Integer> {
 
-    @Query(value = "SELECT A.ID_NOTIFICATION, A.ID_VACA, A.NOMBRE_EVENTO, A.FECHA_EVENTO, A.ESTADO " +
-            "FROM EVENTS A WHERE :fechaEvento", nativeQuery = true)
+    @Query(value = "SELECT ID_NOTIFICATION, ID_VACA, NOMBRE_EVENTO, FECHA_EVENTO, ESTADO FROM EVENTS WHERE FECHA_EVENTO = :fechaEvento", nativeQuery = true)
     List<Events> getByDate(String fechaEvento);
 }
